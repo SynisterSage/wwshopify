c4dc13ef4fbc01301dc18af65be5a51d8491a693
Lex Ferguson <82519402+SynisterSage@users.noreply.github.com>
2025-12-12 09:23:46 -0500

    chore: added storefront info

--- app/components/CartLineItem.jsx
+++ app/components/CartLineItem.jsx
@@ -21,17 +21,27 @@ export function CartLineItem({layout, line}) {
   return (
     <li key={id} className="cart-line">
       {image && (
-        <Image
-          alt={title}
-          aspectRatio="1/1"
-          data={image}
-          height={100}
-          loading="lazy"
-          width={100}
-        />
+        <Link
+          to={lineItemUrl}
+          onClick={() => {
+            if (layout === 'aside') {
+              close();
+            }
+          }}
+          className="cart-line-image"
+        >
+          <Image
+            alt={title}
+            aspectRatio="1/1"
+            data={image}
+            height={100}
+            loading="lazy"
+            width={100}
+          />
+        </Link>
       )}
 
-      <div>
+      <div className="cart-line-details">
         <Link
           prefetch="intent"
           to={lineItemUrl}
@@ -40,22 +50,24 @@ export function CartLineItem({layout, line}) {
               close();
             }
           }}
+          className="cart-line-title"
         >
-          <p>
-            <strong>{product.title}</strong>
-          </p>
+          {product.title}
         </Link>
-        <ProductPrice price={line?.cost?.totalAmount} />
-        <ul>
+        <ul className="cart-line-options">
           {selectedOptions.map((option) => (
             <li key={option.name}>
-              <small>
-                {option.name}: {option.value}
-              </small>
+              {option.name}: {option.value}
             </li>
           ))}
         </ul>
-        <CartLineQuantity line={line} />
+        <div className="cart-line-bottom">
+          <CartLineQuantity line={line} />
+          <div className="cart-line-price">
+            <ProductPrice price={line?.cost?.totalAmount} />
+          </div>
+          <CartLineRemoveButton lineIds={[id]} />
+        </div>
       </div>
     </li>
   );
@@ -75,7 +87,6 @@ function CartLineQuantity({line}) {
 
   return (
     <div className="cart-line-quantity">
-      <small>Quantity: {quantity} &nbsp;&nbsp;</small>
       <CartLineUpdateButton lines={[{id: lineId, quantity: prevQuantity}]}>
         <button
           aria-label="Decrease quantity"
@@ -83,10 +94,10 @@ function CartLineQuantity({line}) {
           name="decrease-quantity"
           value={prevQuantity}
         >
-          <span>&#8722; </span>
+          −
         </button>
       </CartLineUpdateButton>
-      &nbsp;
+      <span className="cart-line-quantity-value">{quantity}</span>
       <CartLineUpdateButton lines={[{id: lineId, quantity: nextQuantity}]}>
         <button
           aria-label="Increase quantity"
@@ -94,11 +105,9 @@ function CartLineQuantity({line}) {
           value={nextQuantity}
           disabled={!!isOptimistic}
         >
-          <span>&#43;</span>
+          +
         </button>
       </CartLineUpdateButton>
-      &nbsp;
-      <CartLineRemoveButton lineIds={[lineId]} disabled={!!isOptimistic} />
     </div>
   );
 }
@@ -109,7 +118,7 @@ function CartLineQuantity({line}) {
  * that it was successfully added to the cart.
  * @param {{
  *   lineIds: string[];
- *   disabled: boolean;
+ *   disabled?: boolean;
  * }}
  */
 function CartLineRemoveButton({lineIds, disabled}) {
@@ -120,8 +129,13 @@ function CartLineRemoveButton({lineIds, disabled}) {
       action={CartForm.ACTIONS.LinesRemove}
       inputs={{lineIds}}
     >
-      <button disabled={disabled} type="submit">
-        Remove
+      <button disabled={disabled} type="submit" className="cart-line-remove" aria-label="Remove from cart">
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+          <polyline points="3 6 5 6 21 6"></polyline>
+          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+          <line x1="10" y1="11" x2="10" y2="17"></line>
+          <line x1="14" y1="11" x2="14" y2="17"></line>
+        </svg>
       </button>
     </CartForm>
   );
